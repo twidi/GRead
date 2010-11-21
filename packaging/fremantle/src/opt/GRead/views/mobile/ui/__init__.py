d8cd4b8956957d7bdc83c0df2c85273e8eb2f604
twidi <s.angel@twidi.com>
2010-11-21 22:18:05 +0100

    packaging version 1.0.6

--- packaging/fremantle/src/opt/GRead/views/mobile/ui/__init__.py
+++ packaging/fremantle/src/opt/GRead/views/mobile/ui/__init__.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from ...basic.ui import Ui_Window as Basic_Ui_Window
+from PyQt4.QtCore import SIGNAL
+from ...basic import ui
 
-class Ui_Window(Basic_Ui_Window): 
+RequestContextMenuSignal = SIGNAL("request_context_menu(QPoint&)")
+
+class ListView(ui.base_listview_class):
+    def __init__(self, *args, **kwargs):
+        super(ListView, self).__init__(*args, **kwargs)
+
+    def mouseReleaseEvent(self, e):
+        if self.indexAt(e.pos()).row() >= 0:
+            max_width = self.width()
+            if self.verticalScrollBar().isVisible():
+                max_width = max_width - self.verticalScrollBar().sizeHint().width()
+            if max_width - e.x() <= 50:
+                self.emit(RequestContextMenuSignal, e.pos())
+                return
+        return super(ListView, self).mouseReleaseEvent(e)
+
+class Ui_Window(ui.Ui_Window):
     pass
     
-from ...basic import ui
 ui.base_ui_window_class = Ui_Window
+ui.base_listview_class  = ListView
